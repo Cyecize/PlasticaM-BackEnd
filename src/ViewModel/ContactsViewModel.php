41acc74d59b3d16866dc3b8447463535e583947c
Tsvetelin Yakimov <ceci2205@abv.bg>
2021-03-06 23:39:16 +0200

    Add more contact info.

--- src/ViewModel/ContactsViewModel.php
+++ src/ViewModel/ContactsViewModel.php
@@ -12,10 +12,16 @@ class ContactsViewModel
 
     private $whatsapp;
 
+    private $facebook;
+
+    private $youtube;
+
     public function __construct(array $settings)
     {
         $this->email = $settings['email'];
         $this->phoneNumber = $settings['phoneNumber'];
         $this->whatsapp = $settings['whatsapp'];
+        $this->facebook = $settings['facebook'];
+        $this->youtube = $settings['youtube'];
     }
 }

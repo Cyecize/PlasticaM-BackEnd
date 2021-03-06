46703057b4683578855f8d7dd376d48651850a78
Tsvetelin Yakimov <ceci2205@abv.bg>
2021-03-06 17:51:17 +0200

    Add global ApiException.

---
+++ src/Exception/ApiException.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Exception;
+
+
+use Throwable;
+
+class ApiException extends \RuntimeException
+{
+    public function __construct($message = "", Throwable $previous = null)
+    {
+        parent::__construct($message, 0, $previous);
+    }
+}
